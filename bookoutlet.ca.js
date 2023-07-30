df3dcd937ccf2ec4b34f0a08161fe0ee96ffc1e2
Bill Tyros <me@strategineer.com>
2023-07-30 15:08:33 -0400

    Update bookoutlet.ca.js

--- bookoutlet.ca.js
+++ bookoutlet.ca.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Bookoutlet GoodReads Link Adder
+// @name         BookOutlet Link to GoodReads when browsing
 // @namespace    http://tampermonkey.net/
-// @version      0.3.0
+// @version      0.3.1
 // @description  try to take over the world!
 // @author       strategineer
 // @match        https://bookoutlet.ca/*
@@ -20,20 +20,22 @@
     }
 
     async function code(ms) {
-        console.log('calling');
+        //console.log('calling');
         const result = await resolveAfterDelay(ms);
-        console.log("Adding links...");
+        //console.log("Adding links...");
         //await new Promise(r => setTimeout(r, 2000));
         const images = document.getElementsByTagName('img');
-        console.log('images:' + images.length);
+        //console.log('images:' + images.length);
         for (var i = 0; i < images.length; i++) {
             try {
                 const img = images[i];
                 let text = "";
                 try {
+                    // On initial page load the ISBN can be found in a noscript block next to the img block (the src field is just data for some reason)
                     text = img.parentElement.childNodes[1].innerText;
                 }
                 catch {
+                    // When navigating to another page of books, the ISBN is found in the src field of the image...
                     text = img.src;
                 }
                 const left = text.indexOf("/");
@@ -46,32 +48,32 @@
                     const link = img.parentElement.parentElement.parentElement.parentElement.parentElement;
                     link.href = url;
                     link.target = "_blank";
-                    console.log(isbn);
-                    console.log(img);
-                    console.log(img.src);
-                    console.log(link);
+                    //console.log(isbn);
+                    //console.log(img);
+                    //console.log(img.src);
+                    //console.log(link);
                 }
                 //console.log(t_str);
                 //console.log(link);
             }
             catch (e) {
                 // ignore errors
-                console.log("ERROR:");
-                console.log(images[i]);
-                console.log(images[i].parentElement.parentElement.parentElement.parentElement.parentElement);
-                console.log(e);
+                //console.log("ERROR:");
+                //console.log( images[i] );
+                //console.log( images[i].parentElement.parentElement.parentElement.parentElement.parentElement );
+                //console.log(e);
             }
         }
-        console.log("Added links!");
+        //console.log("Added links!");
         // Expected output: "resolved"
     }
     window.onload = function () {
-        console.log("onload");
+        //console.log("onload");
         code(0);
     }
     if (window.onurlchange === null) {
         window.addEventListener('urlchange', (info) => {
-            console.log('urlchange');
+            //console.log('urlchange');
             code(1000);
         });
     }
