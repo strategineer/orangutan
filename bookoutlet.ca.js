17aed2e8c4aff735b77824a76dd5d251966544c7
Bill Tyros <me@strategineer.com>
2023-07-30 08:52:15 -0400

    Update bookoutlet.ca.js

--- bookoutlet.ca.js
+++ bookoutlet.ca.js
@@ -7,6 +7,7 @@
 // @match        https://bookoutlet.ca/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bookoutlet.ca
 // @grant        none
+// @run-at document-start
 // @downloadURL  https://raw.githubusercontent.com/strategineer/orangutan/main/bookoutlet.ca.js
 // ==/UserScript==
 // todo: search by isbn
@@ -20,42 +21,17 @@
         for (var i = 0; i < titles.length; i++) {
             const t = titles[i].firstChild;
 
-            const t_str = t.firstChild.nodeValue;
-            if (t == null || t.firstChild == null || t_str == null) {
+            if (t == null || t.firstChild == null || t.firstChild.nodeValue == null) {
                 continue;
             }
-            console.log(t);
+            const t_str = t.firstChild.nodeValue.split("(")[0];
             const author_str = t.parentElement.childNodes[1].childNodes[1].innerText.split(",")[0];
-            console.log(author_str);
             const url = `https://www.goodreads.com/search?utf8=%E2%9C%93&q=${author_str + ' ' + t_str}&search_type=books&search%5Bfield%5D=on`;
             t.parentElement.parentElement.parentElement.href = url;
             t.parentElement.parentElement.parentElement.target = "_blank";
             const img = t.parentElement.parentElement.firstChild.firstChild.firstChild.firstChild;
+            console.log(`book title: ${t_str}, author last name: ${author_str}`);
             console.log(img.src);
-            //console.log(t.parentElement);
-            console.log(t_str);
-            console.log(author_str);
         }
-
-        // Book page
-        const t = document.getElementsByClassName('MuiTypography-root MuiTypography-h1')[0];
-        if (t == null || t.firstChild == null || t.firstChild.nodeValue == null) {
-            return;
-        }
-        //console.log("title on book page found");
-        const url = `https://www.goodreads.com/search?utf8=%E2%9C%93&q=${t.firstChild.nodeValue}&search_type=books&search%5Bfield%5D=on`;
-        let a = document.createElement('a');
-        var link = document.createTextNode("Good Reads");
-        a.appendChild(link);
-        a.title = "Good Reads";
-        a.href = url;
-        a.target = '_blank';
-        try {
-            t.firstChild.appendChild(a);
-        } catch {
-            // ignore error
-        }
-        //console.log(t);
-        //console.log(t.firstChild.nodeValue);
-    };
+    }
 })();
