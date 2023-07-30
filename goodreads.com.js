f42112b296dd2a189d3e06d238135ca901e91a92
Bill Tyros <me@strategineer.com>
2023-07-30 09:21:24 -0400

    Update goodreads.com.js

--- goodreads.com.js
+++ goodreads.com.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Navigate to single search result
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  try to take over the world!
 // @author       strategineer
 // @match        https://www.goodreads.com/search?*
@@ -15,7 +15,7 @@
     'use strict';
     window.onload = function () {
         const check = document.getElementsByClassName("searchSubNavContainer")[0];
-        if (check && check.innerText.includes("Page 1 of about 1 results")) {
+        if (check && (check.innerText.includes("Page 1 of about 1 results") || check.innerText.includes("One result for"))) {
             const a = document.getElementsByClassName("bookCover")[0].parentElement;
             const url = a.href;
             window.open(url, "_self");
