23efb75626ab7b84ccf2a7c14c61fc68b4b22142
Bill Tyros <me@strategineer.com>
2023-07-31 08:43:41 -0400

    Update bookoutlet.ca.js

--- bookoutlet.ca.js
+++ bookoutlet.ca.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         BookOutlet Link to GoodReads when browsing
 // @namespace    http://tampermonkey.net/
-// @version      0.3.5
+// @version      0.4.0
 // @description  try to take over the world!
 // @author       strategineer
 // @match        https://bookoutlet.ca/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bookoutlet.ca
-// @grant        window.onurlchange
+// @grant        none
 // @downloadURL  https://raw.githubusercontent.com/strategineer/orangutan/main/bookoutlet.ca.js
 // ==/UserScript==
 (function () {
@@ -19,36 +19,24 @@
         });
     }
 
-    function parseIsbnFromSrc(text) {
-        const left = text.indexOf("/");
-        const right = text.indexOf(".jpg");
-        let isbn = text.substring(left, right - 2);
-        const rightmost_slash = isbn.lastIndexOf("/");
-        isbn = isbn.substring(rightmost_slash + 1);
-        return isbn;
-    }
-
     function parseIsbnFromProductUrl(url) {
         //https://bookoutlet.ca/products/9781473233782B/children-of-dune-dune-bk-3
-        const products = url.indexOf("products")
-        const left = url.indexOf("/", products);
-        const right = url.indexOf("/", left + 1);
-        return url.substring(left + 1, right - 1);
+        const re = /bookoutlet.ca\/products\/(\d+)/g;
+        const match = re.exec(url);
+        return match[1];
     }
 
     function formatGoodReadsSearchUrlFromIsbn(isbn) {
         return `https://www.goodreads.com/search?utf8=%E2%9C%93&q=${isbn}&search_type=books&search%5Bfield%5D=on`;
     }
-    function setLink(isbn, target) {
-        const url = formatGoodReadsSearchUrlFromIsbn(isbn);
-        target.href = url;
-        target.target = "_blank";
+
+    function isProductUrl(url) {
+        return url.includes("bookoutlet.ca/product");
     }
 
-    function isProductPage() {
-        return window.location.href.includes("bookoutlet.ca/product");
+    function isCurrentlyOnProductPage() {
+        return isProductUrl(window.location.href);
     }
-
     function goToGoodReads() {
         const isbn = parseIsbnFromProductUrl(window.location.href);
         const url = formatGoodReadsSearchUrlFromIsbn(isbn);
@@ -57,46 +45,31 @@
 
     async function code(ms) {
         const result = await resolveAfterDelay(ms);
-        const images = document.getElementsByTagName('img');
-        for (var i = 0; i < images.length; i++) {
+        const anchors = document.getElementsByTagName('a');
+        for (var i = 0; i < anchors.length; i++) {
             try {
-                const img = images[i];
-                let text = "";
-                try {
-                    // On initial page load the ISBN can be found in a noscript block next to the img block (the src field is just data for some reason)
-                    text = img.parentElement.childNodes[1].innerText;
-                }
-                catch {
-                    // When navigating to another page of books, the ISBN is found in the src field of the image...
-                    text = img.src;
-                }
-                const isbn = parseIsbnFromSrc(text);
-                if (isbn) {
-                    const link = img.parentElement.parentElement.parentElement.parentElement.parentElement;
+                const anchor = anchors[i];
+                if (isProductUrl(anchor.href)) {
+                    const isbn = parseIsbnFromProductUrl(anchor.href);
                     const url = formatGoodReadsSearchUrlFromIsbn(isbn);
-                    link.href = url;
-                    link.target = "_blank";
+                    anchor.href = url;
+                    anchor.target = "_blank";
                 }
             }
             catch (e) {
                 // ignore errors
             }
         }
+        if (ms != 0) {
+            code(ms);
+        }
     }
     window.onload = function () {
-        if (isProductPage()) {
+        if (isCurrentlyOnProductPage()) {
             goToGoodReads();
             return
         }
         code(0);
-    }
-    if (window.onurlchange === null) {
-        window.addEventListener('urlchange', (info) => {
-            if (isProductPage()) {
-                goToGoodReads();
-                return;
-            }
-            code(500);
-        });
+        code(3000);
     }
 })();
